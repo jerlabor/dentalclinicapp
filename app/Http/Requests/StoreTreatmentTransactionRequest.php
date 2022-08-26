c1b554ce1c4c0837598987daebac3139ccbea8a5
jerlabor <jertrabaho@gmail.com>
2022-08-26 13:42:52 +0800

    feat: add treatment transaction store request

---
+++ app/Http/Requests/StoreTreatmentTransactionRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTreatmentTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'treatment_process' => 'required|string',
+            'fee'               => 'required|numeric'
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $balance = $this->route()->parameter('treatment')->balance;
+            $fee = $this->input('fee');
+
+            if(($balance - $fee) < 0) {
+                $validator->errors()->add('fee', 'Transactions amount must not exceed total fee.');
+            }
+        });
+    }
+}
