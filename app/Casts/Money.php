6922779ba06c40d94c0b91346b984b68ceca22ad
jerlabor <jertrabaho@gmail.com>
2022-08-26 13:39:37 +0800

    feat: add money cast

---
+++ app/Casts/Money.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class Money implements CastsAttributes
+{
+    private int $decimals;
+
+    public function __construct($decimals = 2)
+    {
+        $this->decimals = $decimals;
+    }
+
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return number_format($value,$this->decimals);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return $value;
+    }
+}
